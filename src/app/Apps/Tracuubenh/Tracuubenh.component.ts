5001f89cfe47f73dbbe3807d79acf4b3fd2d8a2c
Nguyễn Huy Hiệp <nguyenhuyhiep035@gmail.com>
2017-07-27 19:06:12 +0700

    menu left + title search benh

--- src/app/Apps/Tracuubenh/Tracuubenh.component.ts
+++ src/app/Apps/Tracuubenh/Tracuubenh.component.ts
@@ -1,15 +1,25 @@
+import { SettingService } from '../../Share/Services/setting.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-Tracuubenh',
-  templateUrl: './Tracuubenh.component.html',
-  styleUrls: ['./Tracuubenh.component.css']
+    // tslint:disable-next-line:component-selector
+    selector: 'app-Tracuubenh',
+    templateUrl: './Tracuubenh.component.html',
+    styleUrls: ['./Tracuubenh.component.css']
 })
 export class TracuubenhComponent implements OnInit {
+    menu: any;
+    iconText: any;
+    name: any;
+    constructor(private settingService: SettingService) { }
 
-  constructor() { }
+    ngOnInit() {
+        this.menu = this.settingService.getMenu();
+        this.iconText = this.menu[0].items[0].IconText;
+        this.name = this.menu[0].items[0].Ten;
+        console.log(this.name);
+        console.log(this.iconText);
 
-  ngOnInit() {
-  }
+    }
 
 }
--- src/app/Share/Components/window/window.component.ts
+++ src/app/Share/Components/window/window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-window',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./window.component.css']
 })
 export class WindowComponent implements OnInit {
-
+    @Input() name;
+    @Input() icon;
   constructor() { }
 
   ngOnInit() {
