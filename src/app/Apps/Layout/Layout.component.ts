87fbce4b45f41d4ac074b4537aebf928f82d08b7
Nguyễn Huy Hiệp <nguyenhuyhiep035@gmail.com>
2017-07-29 15:26:46 +0700

    click menu left mobile

--- src/app/Apps/Layout/Layout.component.ts
+++ src/app/Apps/Layout/Layout.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+    isToggled = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggled(isToggled) {
+    this.isToggled = isToggled;
+    console.log(this.isToggled);
+  }
 }
