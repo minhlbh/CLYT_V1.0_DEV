786fb3c4a074d9dde1ce806c73f7aade48b6b8d8
Nguyễn Huy Hiệp <nguyenhuyhiep035@gmail.com>
2017-08-02 12:09:31 +0700

    sign in

---
+++ src/app/Share/Services/user.service.ts
@@ -0,0 +1,52 @@
+import { environment } from '../../../environments/environment.prod';
+import { Injectable } from '@angular/core';
+import { URLSearchParams, Http, Response } from '@angular/http';
+
+@Injectable()
+export class UserService {
+    authToken: String = null;
+    tokenType: string = null;
+    private baseUrl = `${environment.apiUrl}`;
+
+    constructor(private http: Http) { }
+
+    // login
+    login(username, password) {
+        // tslint:disable-next-line:prefer-const
+        let body = new URLSearchParams();
+        body.set('username', username);
+        body.set('password', password);
+        body.set('grant_type', 'password');
+        return this.http.post(`http://api.truongkhoa.com/token`, body).map((response: Response) => response.json());
+    }
+
+    // get & set token/localToken
+    setTokenType(token_type: string) {
+        this.tokenType = token_type;
+        localStorage.setItem('token_type', token_type);
+    }
+    setAuthToken(token: string) {
+        this.authToken = token;
+        localStorage.setItem('token', token);
+    }
+    getLocalToken() {
+        return localStorage.getItem('token') ? localStorage.getItem('token') : null;
+    }
+
+    getLocalTokenType() {
+        return localStorage.getItem('token_type') ? localStorage.getItem('token_type') : null;
+    }
+
+    setAuth(data) {
+        localStorage.setItem('auth', JSON.stringify(data));
+    }
+
+    getAuth() {
+        return JSON.parse(localStorage.getItem('auth'));
+    }
+
+    refreshToken() {
+
+    }
+
+}
--- src/app/authModule/SignIn/SignIn.component.ts
+++ src/app/authModule/SignIn/SignIn.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../Share/Services/user.service';
 
 @Component({
-  selector: 'app-SignIn',
-  templateUrl: './SignIn.component.html',
-  styleUrls: ['./SignIn.component.css']
+    selector: 'app-signin',
+    templateUrl: './SignIn.component.html',
+    styleUrls: ['./SignIn.component.css']
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+    error: string = null;
+    username: FormControl = new FormControl();
+    password: FormControl = new FormControl();
 
-  ngOnInit() {
-  }
+    constructor(
+        private router: Router,
+        private userService: UserService
+    ) { }
+
+    ngOnInit() {
+    }
+    // login function
+    onLoginSubmit() {
+        console.log(this.username.value, this.password);
+        this.userService.login(this.username.value, this.password.value).subscribe(
+            (data) => {
+                console.log(data.access_token);
+                console.log(data.token_type);
+                this.userService.setAuthToken(data.access_token);
+                this.userService.setTokenType(data.token_type);
+                this.userService.setAuth(data);
+                this.router.navigate(['']);
+            },
+            err => {
+                const errOb = JSON.parse(err.text());
+                console.log('error:', errOb.error_description);
+                this.error = errOb.error_description;
+            }
+        );
+    }
 
 }
