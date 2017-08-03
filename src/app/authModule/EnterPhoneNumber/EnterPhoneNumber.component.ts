d31de4796518b905d013bf398fb69bb4dac50eec
Nguyễn Huy Hiệp <nguyenhuyhiep035@gmail.com>
2017-08-03 16:43:04 +0700

    check lỗi

--- src/app/authModule/EnterPhoneNumber/EnterPhoneNumber.component.ts
+++ src/app/authModule/EnterPhoneNumber/EnterPhoneNumber.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-enterphonenumber',
-  templateUrl: './EnterPhoneNumber.component.html',
-  styleUrls: ['./EnterPhoneNumber.component.css']
+    selector: 'app-enterphonenumber',
+    templateUrl: './EnterPhoneNumber.component.html',
+    styleUrls: ['./EnterPhoneNumber.component.css']
 })
 export class EnterPhoneNumberComponent implements OnInit {
+    phone: FormControl = new FormControl();
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
