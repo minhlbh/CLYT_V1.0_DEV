22f87b476b4898f6cfd8387ae29a097af58529b5
allstar158 <allstar15897@gmail.com>
2017-08-09 14:27:49 +0700

    commit

--- src/app/Apps/Tracuuthuoc/danh-sach-thuoc/danh-sach-thuoc.component.ts
+++ src/app/Apps/Tracuuthuoc/danh-sach-thuoc/danh-sach-thuoc.component.ts
@@ -3,14 +3,19 @@ import { SettingService } from '../../../Share/Services/setting.service';
 import { Router } from '@angular/router';
 import { WindowComponent } from '../../../Share/Components/window/window.component';
 
+
 @Component({
     selector: 'app-danh-sach-thuoc',
     templateUrl: './danh-sach-thuoc.component.html',
     styleUrls: ['./danh-sach-thuoc.component.css']
 })
 export class DanhSachThuocComponent implements OnInit {
+    menu: any;
     idea: any;
     urlIdea: any;
+    iconText: any;
+    name: any;
+    public url: any;
 
     constructor(
         private router: Router,
@@ -18,6 +23,12 @@ export class DanhSachThuocComponent implements OnInit {
     ) { }
 
     ngOnInit() {
+        this.idea = true;
+        this.urlIdea = 'tracuuthuoc';
+        this.menu = this.settingService.getMenu();
+        this.iconText = this.menu[0].items[1].IconText;
+        this.name = this.menu[0].items[1].Ten;
+        this.url = 'apps';
         this.idea = true;
         this.urlIdea = 'tracuuthuoc';
 
