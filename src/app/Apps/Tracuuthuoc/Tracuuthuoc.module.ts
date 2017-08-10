6762737129b8597142d9324e620189a080acd113
allstar158 <allstar15897@gmail.com>
2017-08-10 10:14:05 +0700

    commit

--- src/app/Apps/Tracuuthuoc/Tracuuthuoc.module.ts
+++ src/app/Apps/Tracuuthuoc/Tracuuthuoc.module.ts
@@ -7,6 +7,8 @@ import { TracuuthuocComponent } from './Tracuuthuoc.component';
 import { DanhSachThuocComponent } from './danh-sach-thuoc/danh-sach-thuoc.component';
 // import { WindowComponent } from '../../Share/Components/window/window.component';
 import { ShareModule } from '../../Share/Components/share.module';
+import { ThuocService } from '../../Share/Services/thuoc.service';
+
 
 
 
@@ -30,6 +32,9 @@ const Routing: ModuleWithProviders = RouterModule.forChild(routing);
     declarations: [TracuuthuocComponent,
         DanhSachThuocComponent,
         // WindowComponent
+    ],
+    providers: [
+        ThuocService,
     ]
 })
 export class TracuuthuocModule {
--- src/app/Apps/Tracuuthuoc/danh-sach-thuoc/danh-sach-thuoc.component.ts
+++ src/app/Apps/Tracuuthuoc/danh-sach-thuoc/danh-sach-thuoc.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { SettingService } from '../../../Share/Services/setting.service';
 import { WindowComponent } from '../../../Share/Components/window/window.component';
-import { Thuoc, ThuocService,  } from '../../../Share/Services/thuoc.service';
+import { Thuoc, ThuocService, } from '../../../Share/Services/thuoc.service';
 
 
 
@@ -22,8 +22,7 @@ export class DanhSachThuocComponent implements OnInit {
     name: any;
     DsThuoc: Thuoc[];
     TongSoLuong: number;
-    startThuoc: number;
-    endThuoc: number;
+
     public url: any;
 
 
@@ -35,28 +34,26 @@ export class DanhSachThuocComponent implements OnInit {
 
     ngOnInit() {
         this.thuocService.getThuoc(1).subscribe(data => {
-            this.DsThuoc = data.DsThuoc;
+            this.DsThuoc = data.DsThuoc.DsThuoc;
+            console.log(this.DsThuoc);
             this.TongSoLuong = data.TongSoLuong;
-            this.startThuoc = 0;
-            this.endThuoc = 50;
-            this.idea = true;
-            this.urlIdea = 'tracuuthuoc';
-            this.menu = this.settingService.getMenu();
-            for (let i = 0; i < this.menu.length; i++) {
-                for (let x = 0; x < this.menu[i].items.length; x++) {
-                    if (this.menu[i].items[x].url === 'tracuuthuoc') {
-                        this.name = this.menu[i].items[x].Ten;
-                        this.iconText = this.menu[i].items[x].IconText;
-                        this.idIdea = this.menu[i].items[x].Id;
-                    }
+        });
+        this.idea = true;
+        this.urlIdea = 'tracuuthuoc';
+        this.menu = this.settingService.getMenu();
+        for (let i = 0; i < this.menu.length; i++) {
+            for (let x = 0; x < this.menu[i].items.length; x++) {
+                if (this.menu[i].items[x].url === 'tracuuthuoc') {
+                    this.name = this.menu[i].items[x].Ten;
+                    this.iconText = this.menu[i].items[x].IconText;
+                    this.idIdea = this.menu[i].items[x].Id;
                 }
             }
-            this.url = 'apps';
-            this.idea = true;
-            this.urlIdea = 'tracuuthuoc';
+        }
+        this.url = 'apps';
+        this.idea = true;
+        this.urlIdea = 'tracuuthuoc';
 
-
-        });
 
     }
 }
