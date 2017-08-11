76adcf148b4a799be6b10ed7b32b899af8c17e39
allstar158 <allstar15897@gmail.com>
2017-08-11 18:22:40 +0700

    commit

---
+++ src/app/Apps/Bantinhl7/Bantinhl7.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bantinhl7',
+  templateUrl: './Bantinhl7.component.html',
+  styleUrls: ['./Bantinhl7.component.css']
+})
+export class Bantinhl7Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
