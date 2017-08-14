8d87cb4391e036dcb9e3614eaffcda6cb3482d12
Nguyễn Huy Hiệp <nguyenhuyhiep035@gmail.com>
2017-08-14 15:45:00 +0700

    left menu fix collapsed

--- src/app/Share/Components/share.module.ts
+++ src/app/Share/Components/share.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Ng2CompleterModule } from 'ng2-completer';
+import { FormsModule } from '@angular/forms';
+
 import { WindowComponent } from './window/window.component';
+import { AutoCompleteComponent } from './AutoComplete/AutoComplete.component';
+
 import { SafePipe } from '../../Share/Pipes/safe.pipe';
+
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        FormsModule,
+        Ng2CompleterModule
     ],
     declarations: [
         WindowComponent,
-        SafePipe
+        SafePipe,
+        AutoCompleteComponent
     ],
     exports: [
         WindowComponent,
+        AutoCompleteComponent,
         SafePipe
     ]
 })
---
+++ src/app/Share/Services/autoComplete.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class AutoCompleteService {
+
+    constructor(
+        private http: Http
+    ) { }
+    private baseUrl = `${environment.apiUrl}`;
+
+    autoComplete(key: String, apiUrl: string) {
+        const autoCompleteUrl = `${this.baseUrl}${apiUrl}?Trang=1&searchTerm=${key}&soluongmoitrang=15`;
+        return this.http.get(autoCompleteUrl)
+            // ...and calling .json() on the response to return data
+            .map((res: Response) => res.json())
+            // ...errors if any
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+
+    }
+}
