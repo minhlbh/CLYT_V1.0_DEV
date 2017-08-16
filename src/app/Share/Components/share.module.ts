db0ab1233472b0ce79e2d19d17d9c2b295b04d97
Nguyễn Huy Hiệp <nguyenhuyhiep035@gmail.com>
2017-08-16 16:05:00 +0700

    a

--- src/app/Share/Components/share.module.ts
+++ src/app/Share/Components/share.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ng2CompleterModule } from 'ng2-completer';
@@ -47,56 +46,3 @@ import { FacebookModule } from 'ngx-facebook';
     ]
 })
 export class ShareModule { }
-=======
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Ng2CompleterModule } from 'ng2-completer';
-import { FormsModule } from '@angular/forms';
-
-import { WindowComponent } from './window/window.component';
-
-import { SafePipe } from '../../Share/Pipes/safe.pipe';
-import { ChiTietBenhComponent } from '../../Apps/Tracuubenh/chi-tiet-benh/chi-tiet-benh.component';
-import { LoadDuLieuImgComponent } from '../../Apps/Tracuubenh/chi-tiet-benh/Load-items/load-du-lieu-img/load-du-lieu-img.component';
-import { LoadDuLieuLinkComponent } from '../../Apps/Tracuubenh/chi-tiet-benh/Load-items/load-du-lieu-link/load-du-lieu-link.component';
-import { LoadDuLieuTextComponent } from '../../Apps/Tracuubenh/chi-tiet-benh/Load-items/load-du-lieu-text/load-du-lieu-text.component';
-// tslint:disable-next-line:max-line-length
-import { LoadDuLieuYoutubeComponent } from '../../Apps/Tracuubenh/chi-tiet-benh/Load-items/load-du-lieu-youtube/load-du-lieu-youtube.component';
-import { ForumBarComponent } from '../../Apps/Tracuubenh/forum-bar/forum-bar.component';
-import { FacebookService } from 'ngx-facebook';
-import { FacebookModule } from 'ngx-facebook';
-import { AutoCompleteComponent } from './autoComplete/autoComplete.component';
-
-
-
-@NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        Ng2CompleterModule,
-        FacebookModule.forRoot()
-    ],
-    declarations: [
-        WindowComponent,
-        SafePipe,
-        ChiTietBenhComponent,
-        LoadDuLieuImgComponent,
-        LoadDuLieuLinkComponent,
-        LoadDuLieuTextComponent,
-        LoadDuLieuYoutubeComponent,
-        ForumBarComponent,
-        AutoCompleteComponent
-    ],
-    exports: [
-        WindowComponent,
-        SafePipe,
-        ChiTietBenhComponent,
-        LoadDuLieuImgComponent,
-        LoadDuLieuLinkComponent,
-        LoadDuLieuTextComponent,
-        LoadDuLieuYoutubeComponent,
-        ForumBarComponent
-    ]
-})
-export class ShareModule { }
->>>>>>> chitietbenh
