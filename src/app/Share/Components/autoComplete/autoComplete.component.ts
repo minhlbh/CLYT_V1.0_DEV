1844adc624155b255a7ddb1cff5a2a2f6654e306
allstar158 <allstar15897@gmail.com>
2017-08-19 15:44:42 +0700

    cm

--- src/app/Share/Components/autoComplete/autoComplete.component.ts
+++ src/app/Share/Components/autoComplete/autoComplete.component.ts
@@ -2,9 +2,9 @@ import { FormControl, NgControl, FormControlDirective, FormGroupDirective } from
 import { Component, OnInit, Injectable, Input } from '@angular/core';
 
 import { AutoCompleteService } from '../../Services/autoComplete.service';
-
 import { CompleterData, CompleterService } from 'ng2-completer';
 import { Subject } from 'rxjs/Subject';
+import { Benh } from '../../Services/benh.service';
 
 
 
@@ -22,7 +22,7 @@ export class AutoCompleteComponent implements OnInit {
     public empty = false;
     public loading = false;
 
-
+    DsBenh: Benh[];
     searchKey = new FormControl('');
     searchUpdate: Subject<string> = new Subject<string>();
     searchData: any;
@@ -34,23 +34,19 @@ export class AutoCompleteComponent implements OnInit {
     constructor(
         private AutoCompleteService: AutoCompleteService,
         private completerService: CompleterService) {
-        this.dataService = completerService.local(this.searchData, 'color', 'color');
         this.searchKey.valueChanges
             .debounceTime(1000)
             .subscribe((event) => {
-                this.doSearchAutoBenh(event);
-                // this.clickThuoc(null);p
-                console.log(this.apiUrl);
-
+                this.doSearchAuto(event);
+                // this.clickThuoc(null);
             });
+            this.dataService = completerService.local(this.DsBenh, 'Name', 'Name');
     }
-    @Injectable()
 
     ngOnInit() {
     }
-    // autocomplete benh
 
-    doSearchAutoBenh(text: string) {
+    doSearchAuto(text: string) {
         // no keyword catched => return all
         if (text === '') {
             this.isSearch = false;
@@ -60,22 +56,21 @@ export class AutoCompleteComponent implements OnInit {
             this.isSearch = true;
             this.loading = true;
             this.searchUpdate.next(text);
-            setTimeout(() => {
-                this.AutoCompleteService.autoComplete(text, this.apiUrl).subscribe(data => {
-                    // this.DsBenh = data.DsBenh.DsBenh;
-                    // this.TongSoLuong = data.DsBenh.TongSoLuong;
-                    // this.startBenh = 0;
-                    // this.endBenh = data.DsBenh.TongSoLuong;
-                    // if (this.DsBenh.length === 0 && this.TongSoLuong === 0) {
-                    //     this.empty = true;
-                    // } else {
-                    //     this.empty = false;
-                    // }
-                    // this.loading = false;
+            console.log(text);
 
+            this.AutoCompleteService.autoComplete(text, this.apiUrl).subscribe(data => {
+                console.log(data);
 
-                });
-            }, 1500);
+                this.DsBenh = data.DsBenh;
+                this.TongSoLuong = data.TongSoLuong;
+                this.endBenh = data.TongSoLuong;
+                if (this.DsBenh.length === 0 && this.TongSoLuong === 0) {
+                    this.empty = true;
+                } else {
+                    this.empty = false;
+                }
+                this.loading = false;
+            });
         }
     }
 }
